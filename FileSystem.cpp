3d8ad65e10dbff1bfaeff5b5d96add840c74d4fd
Fahad AHammed <ahaammed@ualberta.ca>
2019-12-04 23:40:27 -0700

    consistency checks done

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -5,10 +5,11 @@
 #include <vector>
 #include <cstring>
 #include <unistd.h>
+#include <string.h>
 #include <map>
 #include <set> 
+#include <fstream>
 using namespace std;
-
 
 void fs_mount(char *new_disk_name);
 void fs_create(char name[5], int size);
@@ -54,7 +55,7 @@ int main(void){
             strcpy(name,string_input_list[1].c_str() );
             int size = stoi(string_input_list[2]);
             fs_create(name, size);
-            delete[] name;
+            // delete[] name;
         }
         
     }
@@ -65,7 +66,7 @@ int main(void){
             char name[5];
             strcpy(name,string_input_list[1].c_str() );
             fs_delete(name);
-            delete[] name;
+            // delete[] name;
         }
     }
 
@@ -75,7 +76,7 @@ int main(void){
             strcpy(name,string_input_list[1].c_str() );
             int blockNum = stoi(string_input_list[2]);
             fs_read(name, blockNum);
-            delete[] name;
+            // delete[] name;
         }
     }
 
@@ -85,7 +86,7 @@ int main(void){
             strcpy(name,string_input_list[1].c_str() );
             int blockNum = stoi(string_input_list[2]);
             fs_write(name, blockNum);
-            delete[] name;
+            // delete[] name;
         }
     }
 
@@ -93,7 +94,7 @@ int main(void){
         uint8_t buff[1024];
         copy(string_input_list[1].begin(),string_input_list[1].end(),buff);
         fs_buff(buff);
-        delete[] buff;
+        // delete[] buff;
     }
 
     else if(string_input_list[0] == "L" && string_input_list.size() == 1){
@@ -107,7 +108,7 @@ int main(void){
             strcpy(name,string_input_list[1].c_str() );
             int newSize = stoi(string_input_list[2]);
             fs_resize(name, newSize);
-            delete[] name;
+            // delete[] name;
         }
     }
 
@@ -120,7 +121,7 @@ int main(void){
             char name[5];
             strcpy(name,string_input_list[1].c_str() );
             fs_cd(name);
-            delete[] name;
+            // delete[] name;
         }
     }
 
@@ -133,52 +134,31 @@ int main(void){
 
 
 void fs_mount(char *new_disk_name){
-    cout<<"Disk name: "<<new_disk_name<<endl;
-
-    int fp = open(new_disk_name, O_RDWR);
-    if(fp < 0){
-        cerr << "Cannot find disk " << new_disk_name << endl;
-        return;
-    }
-    // Super_block superBlock;
-    uint8_t superBlock[1024];
-    ssize_t superBlock_size = 1024;
-
-    if(lseek(fp,0,SEEK_SET)<0){
-        cout << "failed to lseek" << endl;
-    }
-    if(read(fp,superBlock,superBlock_size) != superBlock_size){
-        cout << "failed to read" << endl;
-    }
-    else{
-        cout << "success" << endl;
+    cout <<"Disk name: "<< new_disk_name << endl;
+    fstream infile;
+    infile.open(new_disk_name);
+    if(infile.fail()){
+        cout << "infile error" << endl;
     }
 
-    Super_block *S1 = new Super_block; 
-    // insert first 16 bytes into the free block list
-    for (int j=0 ; j < 16; j++) {
-        S1->free_block_list[j] = superBlock[j];
-
-        cout << "in the superBlock " << unsigned(superBlock[j]) << endl;
-        cout << "in the structure " << (int)(S1->free_block_list[j]) << endl;
+    Super_block *S1 = new Super_block;
+    infile.read(S1->free_block_list,16);
+    for(uint8_t i =0; i<126; i++){
+        infile.read((char*)&(S1->inode[i].name),5);
+        infile.read((char*)&(S1->inode[i].used_size),1);
+        infile.read((char*)&(S1->inode[i].start_block),1);
+        infile.read((char*)&(S1->inode[i].dir_parent),1);
     }
-    // insert the inodes in the inodes list in the Super_block structure
-    int ind = 0;
-    for(int i=16; i < 1024 ;){
-        Inode newInode;
-        newInode.name[0]= superBlock[i++];
-        newInode.name[1]= superBlock[i++];
-        newInode.name[2]= superBlock[i++];
-        newInode.name[3]= superBlock[i++];
-        newInode.name[4]= superBlock[i++];
 
-        newInode.used_size = superBlock[i++];
-        newInode.start_block = superBlock[i++];
-        newInode.dir_parent =superBlock[i++];
+    // cout << "the free space list" << endl;
+    // for(int i=0; i<16;i++){
+    //     cout << "free space list: " << i << "   " << unsigned(S1->free_block_list[i]) << endl;
+    // }
+    // cout << "the inodes" << endl;
+    // for(int i=0;i<126; i++){
+    //     cout << "name " << S1->inode[i].name << "     used_size " << unsigned(S1->inode[i].used_size) << "    start_block " << unsigned(S1->inode[i].start_block) << "    dir_parent " << unsigned(S1->inode[i].dir_parent) << endl;  
+    // }
 
-        S1->inode[ind] = newInode;
-        ind++;
-    }
 
     // consistency check 1: Blocks that are marked free in the free-space list cannot be allocated to any file. Similarly, blocks
     //                      marked in use in the free-space list must be allocated to exactly one file.
@@ -200,14 +180,18 @@ void fs_mount(char *new_disk_name){
         }  
     }
 
-    for(int i=0;i<126; i++){
-        uint8_t startBlock = S1->inode[i].start_block;
-        uint8_t inUse_byte = S1->inode[i].used_size;
-        int inUse_bit = findBit(inUse_byte, 8);
-        if(inUse_bit == 1){
+    for(int i=0;i<126; i++){                                                                // going through all the inodes and get their startBlocks and sizes which
+        uint8_t startBlock = S1->inode[i].start_block;                                      // gives us the indexs in our free space list and then we check those bits
+        uint8_t inUse_byte = S1->inode[i].used_size;                                        // in our free space list as it should be the only one's with 1's. if this check
+        int inUse_bit = findBit(inUse_byte, 8);                                             // passes then we remove them from our (position , value) map. So all the positions 
+        if(inUse_bit == 1){                                                                 // in our map should have their bits only as 0's. so we iterate through to do the check.
             uint8_t size = inUse_byte << 1;
-            uint8_t size = inUse_byte >> 1;
+            size = size >> 1;
             for(int j=0; j < size; j++ ){
+                if((startBlock+j) > 127){
+                    cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 4)" << endl;
+                    return;
+                }
                 int pos_bit = pos_map.find(startBlock + j)->second;
                 if(pos_bit == 1){
                     pos_map.erase(startBlock + j);
@@ -220,17 +204,42 @@ void fs_mount(char *new_disk_name){
         }
     }
 
-    pos_map.erase(0);
+    pos_map.erase(0);                                                                           // erasing the initial bit in the free space as it will alsways be 1
     for(auto it = pos_map.begin(); it != pos_map.end(); it++){
         if(it->second == 1){
             cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 1)" << endl;
-            break;
+            return;
         }
     }
     
     // consistency check 2: The name of every file/directory must be unique in each directory.
 
-    set<string> dir_names;                                                  // creating a set of directory names
+   // creating a multimap of file/dir names and index of their parent node
+    multimap<string,uint8_t> name_dirMap;
+    for (int i = 0; i < 126 ;i++){
+        Inode temp = S1->inode[i];
+        uint8_t inUse_byte = temp.used_size;
+        int inUse_bit = findBit(inUse_byte, 8);
+        if (inUse_bit == 1){
+            char node_name[5];
+            strncpy(node_name,S1->inode[i].name,5);
+            uint8_t dir_index = S1->inode[i].dir_parent;
+            dir_index = dir_index <<1;
+            dir_index = dir_index >>1;
+
+            auto ret = name_dirMap.equal_range(node_name);
+            for(auto it = ret.first; it != ret.second; ++it){
+                if(it->second == dir_index){
+                    cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 2)" << endl;
+                    return;
+                }
+            }
+            name_dirMap.insert(pair<string,uint8_t>(node_name, dir_index));
+        }
+    }
+
+
+
 
 
     // consistency check 3: If the state of an inode is free, all bits in this inode must be zero. Otherwise, the name attribute stored
@@ -244,11 +253,12 @@ void fs_mount(char *new_disk_name){
             for(int j=0; j<5; j++){                                                                 // check for name
                 if((int)temp.name[j] != 0){
                     cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 3)" << endl;
-                    break;    
+                    return;  
                 }
             }
             if((temp.dir_parent != 0) || (temp.start_block != 0) || (temp.used_size != 0)){         // check for other shit
                 cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 3)" << endl;
+                return;
             }
         }
         if(inUse_bit == 1){                                                                         // occupied
@@ -261,13 +271,13 @@ void fs_mount(char *new_disk_name){
                 }
                 if (name_flag == false){
                     cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 3)" << endl;
+                    return;
                 }
         }
     }
     
     // consistency check 4: The start block of every inode that is marked as a file must have a value between 1 and 127
     //                      inclusive.
-
     for (int i = 0; i < 126 ;i++){
         Inode temp = S1->inode[i];
         uint8_t inUse_byte = temp.used_size;
@@ -279,13 +289,12 @@ void fs_mount(char *new_disk_name){
                 uint8_t startB = temp.start_block;
                 if(startB < 1 || startB > 127){
                     cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 4)" << endl;
+                    return;
                 }
             }
         }
     }
 
-
-    
     // consistency check 5: The size and start block of an inode that is marked as a directory must be zero.
     for (int i = 0; i < 126 ;i++){
         Inode temp = S1->inode[i];
@@ -295,9 +304,12 @@ void fs_mount(char *new_disk_name){
             uint8_t isDir_byte = temp.dir_parent;
             int isDir_bit = findBit(isDir_byte, 8);
             if(isDir_bit == 1){
-                int size = inUse_byte << 1;
+                cout << "is directory inode:" << i << endl;
+                uint8_t size = inUse_byte << 1;
+                size = size >> 1;
                 if(size != 0 || temp.start_block != 0){
                     cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 5)" << endl;
+                    return;
                 }
             }
         }
@@ -311,17 +323,20 @@ void fs_mount(char *new_disk_name){
         int inUse_bit = findBit(inUse_byte, 8);
         if (inUse_bit == 1){
             uint8_t parent_byte = temp.dir_parent;
+            parent_byte = parent_byte << 1;
             parent_byte = parent_byte >> 1;
             if(parent_byte == 126){
-                cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 5)" << endl;
+                cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 6)" << endl;
+                return;
             }
-            if(parent_byte => 0 && parent_byte <= 125){
+            if(parent_byte >= 0 && parent_byte <= 125){
                 uint8_t inUse_byte2 = S1->inode[parent_byte].used_size;
                 int inUse_bit2 = findBit(inUse_byte2, 8);
                 uint8_t isDir_byte = S1->inode[parent_byte].dir_parent;
                 int isDir_bit = findBit(isDir_byte, 8);
                 if(inUse_bit2 != 1 || isDir_bit != 1){
-                    cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 5)" << endl;
+                    cout << "Error: File system in " << new_disk_name << " is inconsistent (error code: 6)" << endl;
+                    return;
                 }
 
             }
@@ -397,8 +412,11 @@ vector<string> tokenize(const string &str, const char *delim) {
 
 int findBit(uint8_t byteFlag, int whichBit)
 {
-    if (whichBit > 0 && whichBit <= 8)
-        return (byteFlag & (1<<(whichBit-1)));
+    if (whichBit > 0 && whichBit <= 8){
+        if( (byteFlag & (1<<(whichBit-1))) != 0){
+            return 1;
+        }
+    }
     else
         return 0;
 }
